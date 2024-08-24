8cd0d75cfe4d17c9b65cd34f277e84320ca44712
Adam Steven <93397904+SebbyMortimer@users.noreply.github.com>
2024-08-24 15:34:49 +0100

    Website overhaul 2

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-/* document.addEventListener('DOMContentLoaded', function() {
-	const audio = document.getElementById('background-music');
-	audio.volume = 0.5;
-
-	function playMusic() {
-		// Ensure this function runs after a meaningful user interaction
-		audio.play().then(() => {
-			console.log('Autoplay allowed');
-			// Remove event listeners after the first successful interaction
-			document.removeEventListener('click', playMusic);
-			document.removeEventListener('keydown', playMusic);
-		}).catch(error => {
-			console.error('Autoplay was prevented:', error);
-		});
-	}
-
-	// Add event listeners for user interaction
-	document.addEventListener('click', playMusic);
-	document.addEventListener('keydown', playMusic);
-}); */
-
 function playAudio(img) {
 	const audio = document.getElementById('background-music');
 	const source = document.getElementById('music-source');
