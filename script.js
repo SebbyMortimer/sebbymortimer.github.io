fda706c026bd9a463ea4c96994dce0266ddba180
Adam Steven <93397904+SebbyMortimer@users.noreply.github.com>
2024-11-28 21:51:27 +0000

    Updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function playAudio(img) {
-	const audio = document.getElementById('background-music');
-	const source = document.getElementById('music-source');
+	const audio = document.getElementById("background-music");
+	const source = document.getElementById("music-source");
 	
-	source.src = "../audio/".concat(img.src.replace(/^.*[\\\/]/, '').replace("jpg", "mp3"));
+	source.src = "../audio/" + img.src.replace(/^.*[\\\/]/, "").replace("jpg", "mp3");
 	audio.load();
 	audio.play();
 }
