651ece19262e29d1a773c663e96c3f3244caa325
Sebby Mortimer <adamstn@icloud.com>
2025-10-31 22:18:28 +0000

    Added halloween theme, currently always active

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function playAudio(img) {
 	audio.load();
 	audio.play();
 }
+
+function checkSeasonal() {
+    const d = new Date();
+    //if (d.getMonth() == 9 && d.getDate() > 24 && d.getDate() < 32) { // check if month is october (getMonth starts at 0) and its the week leading to halloween
+        document.body.style.setProperty("--bg-image", 'url("images/SteampunkAirshipHalloween.gif")');
+        document.body.style.color = "#d70000";
+        document.querySelectorAll('h1, h2').forEach(tag => {
+            tag.style.fontFamily = "MonsterMash";
+            tag.style.textShadow = "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+        });
+        document.querySelectorAll('p, ul').forEach(tag => {
+            tag.style.color = "#af6034";
+        });
+        document.querySelectorAll('main, .textBackground').forEach(tag => {
+            tag.style.backgroundColor = "#160025";
+        });
+        document.querySelector('footer').style.backgroundColor = "#000000";
+    //}
+}
+
+checkSeasonal();
