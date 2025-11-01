daffc866e93c2acb09bdc133914616e76c889891
Sebby Mortimer <adamstn@icloud.com>
2025-11-01 12:07:41 +0000

    Made heading halloween colours more orange

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function checkSeasonal() {
     const d = new Date();
     //if (d.getMonth() == 9 && d.getDate() > 24 && d.getDate() < 32) { // check if month is october (getMonth starts at 0) and its the week leading to halloween
         document.body.style.setProperty("--bg-image", 'url("images/SteampunkAirshipHalloween.gif")');
-        document.body.style.color = "#d70000";
+        document.body.style.color = "#d72700";
         document.querySelectorAll('h1, h2').forEach(tag => {
             tag.style.fontFamily = "MonsterMash";
             tag.style.textShadow = "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
